40a58c0d90fd77bd450af30de2609e3744581ddd
NiBo <nibocn@gmail.com>
2020-11-25 15:58:12 +0800

    refactor(nacos): 使用配置方式加载 nacos 不使用注解

--- spring-boot-nacos/src/main/java/me/nibo/springboot/nacos/NacosApplication.java
+++ spring-boot-nacos/src/main/java/me/nibo/springboot/nacos/NacosApplication.java
@@ -1,14 +1,10 @@
 package me.nibo.springboot.nacos;
 
-import com.alibaba.nacos.api.config.ConfigType;
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SuppressWarnings({ "PMD.UseUtilityClass" })
 @SpringBootApplication
-@NacosPropertySource(dataId = "example", type = ConfigType.YAML, autoRefreshed = true)
 public class NacosApplication {
 
     public static void main(String[] args) {
